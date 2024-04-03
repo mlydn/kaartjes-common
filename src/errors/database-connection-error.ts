cf0f49f5d1bd008c0940567ab004abf4cff82d1e
mlydn <59616364+mlydn@users.noreply.github.com>
2024-04-03 11:33:24 +0200

    migrate middleware and errors from auth to common

---
+++ src/errors/database-connection-error.ts
@@ -0,0 +1,16 @@
+import { CustomError } from './custom-error'
+
+export class DatabaseConnectionError extends CustomError {
+  statusCode = 500
+  reason = 'Error connecting to database'
+
+  constructor() {
+    super('Error connecting to database')
+    // Required when extending a built in class
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype)
+  }
+
+  serialiseErrors() {
+    return [{ message: this.reason }]
+  }
+}
